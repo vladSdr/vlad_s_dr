1a41370ea1aa12114026a259b1c9f9e726bb9c31
Anastasia <1epova.aa@gmail.com>
2022-07-19 22:50:50 +0300

    new commit

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,9 @@ const firstEcran = document.querySelector('.main-first');
 const secondEcran = document.querySelector('.main-second');
 
 firstButton.addEventListener('click', () => {
-    firstEcran.classList.add('transfer')
-    secondEcran.classList.add('active')
+    firstEcran.classList.add('transfer');
+    firstEcran.classList.remove('active');
+    secondEcran.classList.add('active');
 })
 
 const secondButton = document.querySelector('.main-second-link');
@@ -12,6 +13,7 @@ const thirdEcran = document.querySelector('.main-third');
 
 secondButton.addEventListener('click', () => {
     secondEcran.classList.add('transfer')
+    secondEcran.classList.remove('active');
     thirdEcran.classList.add('active')
 })
 
@@ -23,92 +25,101 @@ const question4 = document.querySelector('.questions-four');
 
 
 const forma1 = document.querySelector('.form-1');
+const continueButton1 = document.querySelector('.continue-1');
 
 if (forma1) {
     forma1.addEventListener('submit', (e) => {
         e.preventDefault();
 
         let inpValue = forma1.querySelector('input').value;
-        let continueButton = question1.querySelector('.continue');
+        
 
         if (inpValue == '0') {
             console.log('first num received!')
-            continueButton.classList.add('working')
+            continueButton1.classList.add('working')
         } else {
             console.log('Wrong!')
         }
 
-        continueButton.addEventListener('click', () => {
+        continueButton1.addEventListener('click', () => {
             question1.classList.remove('open')
             question2.classList.add('open')
+            continueButton1.classList.remove('working')
         })
     });
 }
 const forma2 = document.querySelector('.form-2');
+const continueButton2 = document.querySelector('.continue-2');
 
 if (forma2) {
     forma2.addEventListener('submit', (e) => {
         e.preventDefault();
 
         let inpValue = forma2.querySelector('input').value;
-        let continueButton = question2.querySelector('.continue');
+        
 
         if (inpValue == '3') {
             console.log('second num received!')
-            continueButton.classList.add('working')
+            continueButton2.classList.add('working')
         } else {
             console.log('Wrong!')
         }
 
-        continueButton.addEventListener('click', () => {
+        continueButton2.addEventListener('click', () => {
             question2.classList.remove('open')
             question3.classList.add('open')
+            continueButton2.classList.remove('working')
         })
     });
 }
 const forma3 = document.querySelector('.form-3');
+const continueButton3 = document.querySelector('.continue-3');
 
 if (forma3) {
     forma3.addEventListener('submit', (e) => {
         e.preventDefault();
 
         let inpValue = forma3.querySelector('input').value;
-        let continueButton = question3.querySelector('.continue');
+        
 
         if (inpValue == '8') {
             console.log('third num received!')
-            continueButton.classList.add('working')
+            continueButton3.classList.add('working')
         } else {
             console.log('Wrong!')
         }
 
-        continueButton.addEventListener('click', () => {
+        continueButton3.addEventListener('click', () => {
             question3.classList.remove('open')
             question4.classList.add('open')
+            continueButton3.classList.remove('working')
         })
     });
 }
 const forma4 = document.querySelector('.form-4');
 const fourthEcran = document.querySelector('.main-fourth');
+const continueButton4 = document.querySelector('.continue-4');
 
 if (forma4) {
     forma4.addEventListener('submit', (e) => {
         e.preventDefault();
 
         let inpValue = forma4.querySelector('input').value;
-        let continueButton = question4.querySelector('.continue');
+        
 
         if (inpValue == '7') {
             console.log('fourth num received!')
-            continueButton.classList.add('working')
+            continueButton4.classList.add('working')
         } else {
             console.log('Wrong!')
         }
 
-        continueButton.addEventListener('click', () => {
+        continueButton4.addEventListener('click', () => {
             question4.classList.remove('open')
             thirdEcran.classList.add('transfer')
+            thirdEcran.classList.remove('active');
             fourthEcran.classList.add('active')
+            continueButton4.classList.remove('working')
         })
     });
 }
